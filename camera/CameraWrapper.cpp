CyanogenMod/android_device_motorola_msm8226-common

msm8226-common: camera: Update parameters
- Remove the Motorola specific parameters. Only the Motorola camera
  app uses these and it does so regardless of what the HAL reports.
  the unneeded/unused parameters.

- Clean up the extra parameters list

- Add support for the following parameters:
  - Red eye prevention
  - Noise reduction
  - ZSL (titan/thea)

Change-Id: Ia2b4d5251750c255e4e72bffe3073e84607e0ff3

camera/CameraWrapper.cpp
@@ -147,10 +147,12 @@ static char *camera_fixup_getparams(int id, const char *settings)
 #endif
 
     if (id == BACK_CAMERA) {
-        params.set(CameraParameters::KEY_QC_SUPPORTED_TOUCH_AF_AEC, "touch-on,touch-off");
+        params.set(CameraParameters::KEY_SUPPORTED_FLASH_MODES, "auto,on,off,torch");
     }
 
+    params.set(CameraParameters::KEY_QC_SUPPORTED_DENOISE, "denoise-off,denoise-on");
     params.set(CameraParameters::KEY_QC_SUPPORTED_FACE_DETECTION, "on,off");
+    params.set(CameraParameters::KEY_QC_SUPPORTED_REDEYE_REDUCTION, "enable,disable");
 
     if (get_product_device() == FALCON || get_product_device() == PEREGRINE) {
         if (id == BACK_CAMERA) {
@@ -160,11 +162,12 @@ static char *camera_fixup_getparams(int id, const char *settings)
     } else {
         params.set(CameraParameters::KEY_QC_SUPPORTED_HFR_SIZES, "1296x728,1296x728,720x480");
         params.set(CameraParameters::KEY_QC_SUPPORTED_VIDEO_HIGH_FRAME_RATE_MODES, "60,90,120,off");
+        params.set(CameraParameters::KEY_QC_SUPPORTED_ZSL_MODES, "on,off");
     }
 
     if (!(get_product_device() == FALCON || get_product_device() == PEREGRINE) ||
             id == BACK_CAMERA) {
-        /* The FFC of falcon and peregrine doesn't support scene modes */
+        params.set(CameraParameters::KEY_QC_SUPPORTED_TOUCH_AF_AEC, "touch-on,touch-off");
         params.set(CameraParameters::KEY_SUPPORTED_SCENE_MODES,
                 "auto,action,portrait,landscape,night,night-portrait,theatre"
                 "candlelight,beach,snow,sunset,steadyphoto,fireworks,sports,party,"

AOKP/device_motorola_msm8226-common

camera/CameraWrapper.cpp
@@ -147,10 +147,12 @@ static char *camera_fixup_getparams(int id, const char *settings)
 #endif
 
     if (id == BACK_CAMERA) {
-        params.set(CameraParameters::KEY_QC_SUPPORTED_TOUCH_AF_AEC, "touch-on,touch-off");
+        params.set(CameraParameters::KEY_SUPPORTED_FLASH_MODES, "auto,on,off,torch");
     }
 
+    params.set(CameraParameters::KEY_QC_SUPPORTED_DENOISE, "denoise-on,denoise-off");
     params.set(CameraParameters::KEY_QC_SUPPORTED_FACE_DETECTION, "on,off");
+    params.set(CameraParameters::KEY_QC_SUPPORTED_REDEYE_REDUCTION, "enable,disable");
 
     if (get_product_device() == FALCON || get_product_device() == PEREGRINE) {
         if (id == BACK_CAMERA) {
@@ -160,11 +162,12 @@ static char *camera_fixup_getparams(int id, const char *settings)
     } else {
         params.set(CameraParameters::KEY_QC_SUPPORTED_HFR_SIZES, "1296x728,1296x728,720x480");
         params.set(CameraParameters::KEY_QC_SUPPORTED_VIDEO_HIGH_FRAME_RATE_MODES, "60,90,120,off");
+        params.set(CameraParameters::KEY_QC_SUPPORTED_ZSL_MODES, "on,off");
     }
 
     if (!(get_product_device() == FALCON || get_product_device() == PEREGRINE) ||
             id == BACK_CAMERA) {
-        /* The FFC of falcon and peregrine doesn't support scene modes */
+        params.set(CameraParameters::KEY_QC_SUPPORTED_TOUCH_AF_AEC, "touch-on,touch-off");
         params.set(CameraParameters::KEY_SUPPORTED_SCENE_MODES,
                 "auto,action,portrait,landscape,night,night-portrait,theatre"
                 "candlelight,beach,snow,sunset,steadyphoto,fireworks,sports,party,"